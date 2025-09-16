0f9a673f56a8810a6558d683a7cd5a1f037cb12c
zhangjianuothegreat <1137866514@qq.com>
2025-09-16 17:48:31 +0800

    Remove unnecessary files (pip wheel, python-version.txt, build.sh)

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo "Installing Python 3.12.11"
-/app/.heroku/python/bin/python --version
-echo "Installing pip from local whl"
-/app/.heroku/python/bin/python -m ensurepip
-/app/.heroku/python/bin/pip install ./wheels/pip-24.2-py3-none-any.whl
-echo "Installing requirements"
-/app/.heroku/python/bin/pip install -r requirements.txt
